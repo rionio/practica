0da40338072a30714cf3ee37d62588757d047c7a
rionio <j.abarca.beretta@gmail.com>
2021-11-15 13:06:41 -0300

    Delete unnecesary import

--- practica-backend/src/modules/registro/registro.service.ts
+++ practica-backend/src/modules/registro/registro.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MapperService } from '../../shared/mapper.service';
-import { Any } from 'typeorm';
 import { RegistroDto } from './dto/registro.dto';
 import { Registro } from './registro.entity';
 import { RegisterRepository } from './registro.repository';
