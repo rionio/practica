c3a999b2de02d2076e6d1177b2400e99e2905342
rionio <j.abarca.beretta@gmail.com>
2021-12-14 02:26:59 -0300

    General Changes

--- practica-backend/src/main.ts
+++ practica-backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); //metodo principal de nestjs
-  app.setGlobalPrefix('api'); //se debe conectar a la pagina bajo el prefijo http://localhost:port/api
+  //app.setGlobalPrefix('api'); //se debe conectar a la pagina bajo el prefijo http://localhost:port/api
   app.enableCors();
   await app.listen(AppModule.port); //con AppModule.port se puede configurar el puerto de manera dinamica
   console.log(`Listening on por ${await app.getUrl()}`);
---
+++ practica-backend/src/modules/registro/interface.ts
@@ -0,0 +1,5 @@
+export interface Options {
+  company?: number;
+  user?: number;
+  inter?: number;
+}
